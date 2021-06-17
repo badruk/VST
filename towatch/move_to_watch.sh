ac73d90221d825f4b6d6f98f9a4258392ebfde7c
Davide Barbieri <suxdavide@gmail.com>
2021-06-17 11:17:35 +0200

    Little fix

--- towatch/move_to_watch.sh
+++ towatch/move_to_watch.sh
@@ -2,10 +2,6 @@
 
 # rsyncs the folder "yta" and the contents of the folder Music on the amazift
 
-#YTA = /home/badruk/Music/youtube-dl
-
-#rm -rf /run/user/1000/gvfs/mtp\:host\=Huami_Amazfit_Pace_a89d110a/Memoria\ interna/Music/
-
 rsync -rP --exclude old --delete --ignore-existing /home/badruk/Music/youtube-dl/ /run/user/1000/gvfs/mtp\:host\=Huami_Amazfit_Pace_a89d110a/Memoria\ interna/Music/
 
 # after syncing moves all files from "yta" to "yta/old"
